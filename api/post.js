06800d77e946759929d634b2ffa1371411273c03
Rolly Maulana Awangga <awangga@gmail.com>
2023-09-27 20:42:38 +0700

    coba import default

--- api/post.js
+++ api/post.js
@@ -1,23 +1,5 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
+import PostSignUp from "./postfunction.js";
 
 window.PostSignUp = PostSignUp;
-
-function PostSignUp(){
-    let target_url = "https://eol0j1lmdtbpzp.m.pipedream.net";
-    let tokenkey = "token";
-    let tokenvalue = "8e87pod9d9a8fh9sfd87f9dhsf98dsf98sdf9ssd98f";
-    let datainjson = {
-        "namadepan": getValue("fname"),
-        "namabelakang": getValue("lname")
-    }
-
-    postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
-
-}
-
-
-
-function responseData(result){
-    setInner("pesan",result.success);
-}
