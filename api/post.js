4ec24b4b53857113f37ca61455dbcd253ab52117
Rolly Maulana Awangga <awangga@gmail.com>
2023-09-27 19:24:12 +0700

    pipedream

--- api/post.js
+++ api/post.js
@@ -3,7 +3,7 @@ import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 
 
 export function PostSignUp(){
-    let target_url = "https://postman-echo.com/post";
+    let target_url = "https://eol0j1lmdtbpzp.m.pipedream.net";
     let tokenkey = "token";
     let tokenvalue = "8e87pod9d9a8fh9sfd87f9dhsf98dsf98sdf9ssd98f";
     let datainjson = {
