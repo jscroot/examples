43181480d7f1b67031eb3e5789111f551c35a279
Rolly Maulana Awangga <awangga@gmail.com>
2023-09-27 19:09:23 +0700

    coba post

---
+++ api/post.js
@@ -0,0 +1,22 @@
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
+
+
+export function PostSignUp(){
+    let target_url = "https://postman-echo.com/post";
+    let tokenkey = "token";
+    let tokenvalue = "8e87pod9d9a8fh9sfd87f9dhsf98dsf98sdf9ssd98f";
+    let datainjson = {
+        "namadepan": getValue("fname"),
+        "namabelakang": getValue("lname")
+    }
+
+    postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
+
+}
+
+
+
+function responseData(result){
+    setInner("pesan",result.message);
+}
