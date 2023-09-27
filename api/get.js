a503d244be70d8f0f011aa9a5524fdcb4fbf3556
Rolly Maulana Awangga <awangga@gmail.com>
2023-09-27 17:33:42 +0700

    addChild

--- api/get.js
+++ api/get.js
@@ -1,5 +1,5 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
-import {setInner } from "https://jscroot.github.io/element/croot.js";
+import {setInner,addChild } from "https://jscroot.github.io/element/croot.js";
 
 let URLGeoJson = "https://jscroot.github.io/examples/api/data.json";
 
