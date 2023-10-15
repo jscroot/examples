fbf5147278da33e784c85096e36132ca45c734c3
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-15 12:26:28 +0700

    form

--- api/post/togcf/postfunction.js
+++ api/post/togcf/postfunction.js
@@ -2,12 +2,12 @@ import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 
 export default function PostSignUp(){
-    let target_url = "https://eol0j1lmdtbpzp.m.pipedream.net";
+    let target_url = "https://asia-southeast2-awangga.cloudfunctions.net/petapedia-post";
     let tokenkey = "token";
     let tokenvalue = "8e87pod9d9a8fh9sfd87f9dhsf98dsf98sdf9ssd98f";
     let datainjson = {
-        "namadepan": getValue("fname"),
-        "namabelakang": getValue("lname")
+        "username": getValue("username"),
+        "password": getValue("password")
     }
 
     postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
