ad484d357b1ef692e12847231b4d05013f2f490a
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-15 12:30:30 +0700

    set cookie

--- api/post/togcf/postfunction.js
+++ api/post/togcf/postfunction.js
@@ -1,5 +1,6 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
+import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp(){
     let target_url = "https://asia-southeast2-awangga.cloudfunctions.net/petapedia-post";
@@ -18,4 +19,5 @@ export default function PostSignUp(){
 
 function responseData(result){
     setInner("pesan",result.message);
+    setCookieWithExpireHour("token",result.token,2);
 }
