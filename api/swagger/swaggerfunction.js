733e112c626845b47e267d27f5ffacd8d0f93cea
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-23 06:59:42 +0700

    URLData

--- api/swagger/swaggerfunction.js
+++ api/swagger/swaggerfunction.js
@@ -1,8 +1,12 @@
 import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
 
+
+
+export let URLData = "./openapi.yaml";
+
 export function setSwagger() {
     const ui = SwaggerUIBundle({
-        url: "./openapi.yaml", //Location of Open API spec in the repo
+        url: URLData, //Location of Open API spec in the repo
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
