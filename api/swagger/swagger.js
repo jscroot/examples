b3fcdf9b8d968eba220c13752c979d5a9319b9a6
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-23 06:57:17 +0700

    pisah fungsi

--- api/swagger/swagger.js
+++ api/swagger/swagger.js
@@ -1,19 +1,4 @@
-import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
+import setSwagger from './swaggerfunction.js';
 
 window.onload = setSwagger();
 
-export function setSwagger() {
-    const ui = SwaggerUIBundle({
-        url: "./openapi.yaml", //Location of Open API spec in the repo
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-        presets: [
-            SwaggerUIBundle.presets.apis,
-            SwaggerUIBundle.SwaggerUIStandalonePreset
-        ],
-        plugins: [
-            SwaggerUIBundle.plugins.DownloadUrl
-        ],
-    })
-    return ui
-}
---
+++ api/swagger/swaggerfunction.js
@@ -0,0 +1,17 @@
+import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
+
+export function setSwagger() {
+    const ui = SwaggerUIBundle({
+        url: "./openapi.yaml", //Location of Open API spec in the repo
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIBundle.SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+        ],
+    })
+    return ui
+}
