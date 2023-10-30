f353f979f0a9133811c275d3d5cb1b77d3d17a49
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-30 10:48:15 +0700

    ga bisa pake layout

--- api/swagger/swagger.js
+++ api/swagger/swagger.js
@@ -1,8 +1,4 @@
 import {setSwagger,UIData} from './swaggerfunction.js';
-import SwaggerUIStandalonePreset from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-standalone-preset.js';
-import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
 
-window.SwaggerUIBundle=SwaggerUIBundle;
-window.SwaggerUIStandalonePreset=SwaggerUIStandalonePreset;
 window.ui = UIData;
 window.onload = setSwagger();
--- api/swagger/swaggerfunction.js
+++ api/swagger/swaggerfunction.js
@@ -13,8 +13,7 @@ export const UIData = SwaggerUIBundle({
     ],
     plugins: [
         SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    ]
 });
 
 export function setSwagger() {
