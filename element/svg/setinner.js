f253ae9ea908d95c26351f89d2605f58059c6c04
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-09 20:11:12 +0700

    cuba

--- element/svg/setinner.js
+++ element/svg/setinner.js
@@ -1,13 +1,32 @@
 import { setInner } from "https://jscroot.github.io/element/croot.js";
 import * as croot from "https://jscroot.github.io/element/croot.js";
-import textToSvg from 'https://cdn.skypack.dev/text-to-svg';
 
-const TextToSVG = require('text-to-svg');
-const textToSVG = TextToSVG.loadSync();
-const attributes = {fill: 'red', stroke: 'black'};
-const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
-
-const svg = textToSVG.getSVG('hello', options);
 
 setInner("demo",svg);
 croot.setInner("demo2",svg);
+
+updateCanvas("apa maksude","myCanvas");
+
+function updateCanvas(text,id) {
+    var c = document.getElementById(id);
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 400, 200);
+    ctx.fillStyle = "#212121";
+    ctx.fillRect(0, 0, 400, 200)
+    var gradient = ctx.createLinearGradient(0, 0, 200, 200);
+    gradient.addColorStop(0, '#39FF14');
+    gradient.addColorStop(1, 'white');
+    ctx.fillStyle = gradient;
+    var fontface = "Courier";
+    ctx.font = "30px Courier";
+    ctx.textAlign = 'center';
+    // start with a large font size
+      var fontsize=300;
+      // lower the font size until the text fits the canvas
+      do{
+          fontsize--;
+          ctx.font=fontsize+"px "+fontface;
+      }while(ctx.measureText(text).width>c.width)
+    ctx.fillText(text, 150, 100);
+    console.log(ctx.measureText(text).width);
+  }
