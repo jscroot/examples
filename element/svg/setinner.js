54657da1cada6ed7544e49fdc188f131c9a660e9
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-09 19:49:22 +0700

    ok

--- element/svg/setinner.js
+++ element/svg/setinner.js
@@ -1,7 +1,8 @@
 import { setInner } from "https://jscroot.github.io/element/croot.js";
 import * as croot from "https://jscroot.github.io/element/croot.js";
-import textToSvg from 'https://cdn.skypack.dev/text-to-svg';
+import TextToSVG from 'https://cdn.skypack.dev/text-to-svg';
 
+const textToSVG = TextToSVG.loadSync();
 const attributes = {fill: 'red', stroke: 'black'};
 const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
 
