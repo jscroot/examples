89ca32d9e203465b93679e777a0daebd33e16e2d
Rolly Maulana Awangga <awangga@gmail.com>
2025-08-01 12:51:43 +0700

    Update index.js

--- core/login/index.js
+++ core/login/index.js
@@ -1,7 +1,16 @@
-import {setCookieWithExpireHour} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.8/cookie.js";
-import {onClick,getValue} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.8/element.js";
-import {postJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.8/api.js";
-import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.8/url.js";
+import {setCookieWithExpireHour} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.8/cookie.min.js";
+import {onClick,getValue,addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.8/element.min.js";
+import {postJSON,getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.8/api.min.js";
+//import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.8/url.min.js";
+
+//start import Sweet Alert
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
+await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
+//end sweet alert import
+
+//config param
+const urlBackend = "https://n-pts-yang-contamination.trycloudflare.com";
+
 
 onClick("submit",PostSignIn);
 
@@ -10,10 +19,15 @@ function PostSignIn(){
         "username": getValue("username"),
         "password": getValue("password")
     }
-    postJSON("https://labs-receipt-produced-ty.trycloudflare.com",datainjson,responseFunction);
+    postJSON(urlBackend+"/login",datainjson,responseFunction);
 }
 
 function responseFunction(result){
     console.log(result);
     setCookieWithExpireHour("token",result.data.token,2);
+    getJSON(urlBackend+"/login", runafterGetUsername, "Token", result.data.token);
+}
+
+function runafterGetUsername(result){
+    Swal.fire(result.data.status, result.data.status.message + " : "+result.data.username, 'info');//success,warning,info,question
 }
